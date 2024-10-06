791c8e6597537c2abfd07d7859d9439be88fe9b0
M.S <mohamedseedk908@gmail.com>
2024-10-06 14:46:41 -0700

    first commit

---
+++ android/app/src/main/kotlin/com/doctor/doctor/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.doctor.doctor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DocTor());
+}
+
+class DocTor extends StatelessWidget {
+  const DocTor({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+    );
+  }
+}
+
